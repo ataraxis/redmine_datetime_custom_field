f49287ad4dfefece3c8638ff2db708225b7775a4
Shepherd McIlroy <github@duxfu.net>
2025-04-30 17:06:31 -0600

    Switch to YYYY-MM-DD date format

--- lib/redmine_datetime_custom_field/application_helper_patch.rb
+++ lib/redmine_datetime_custom_field/application_helper_patch.rb
@@ -1,7 +1,7 @@
 require_dependency 'application_helper'
 
 unless Rails.env.test?
-  Date::DATE_FORMATS[:default] = '%d/%m/%Y'
+#  Date::DATE_FORMATS[:default] = '%d/%m/%Y'
 end
 
 module RedmineDatetimeCustomField
@@ -27,7 +27,7 @@ module RedmineDatetimeCustomField
     def calendar_for(field_id, showHours = nil)
       include_calendar_headers_tags
       javascript_tag("$(function() {" +
-                       (showHours ? "datetimepickerOptions.timepicker=true; datetimepickerOptions.format='d/m/Y H:i';" : "datetimepickerOptions.timepicker=false;datetimepickerOptions.format='d/m/Y';") +
+                       (showHours ? "datetimepickerOptions.timepicker=true; datetimepickerOptions.format='Y-m-d H:i';" : "datetimepickerOptions.timepicker=false;datetimepickerOptions.format='Y-m-d';") +
                        "datetimepickerCreate('##{field_id}');" +
                        "$('.custom_field_show_hours').click( function(){ " +
                        "if($('##{field_id}').val()=='') return;" +
@@ -55,13 +55,13 @@ module RedmineDatetimeCustomField
           jquery_locale = l('jquery.locale', :default => current_language.to_s)
           tags << javascript_tag(
             "jQuery.datetimepicker.setLocale('#{jquery_locale}');" +
-              "var datetimepickerOptions={format: 'd/m/Y', dayOfWeekStart: #{start_of_week}," +
+              "var datetimepickerOptions={format: 'Y-m-d', dayOfWeekStart: #{start_of_week}," +
               "scrollInput:false," +
               "closeOnDateSelect:true," +
               "id:'datetimepicker'," +
               "onShow: function( currentDateTime ){" +
               "if( $('#custom_field_show_hours_yes').length==0 ) return;" +
-              "this.setOptions( { format: ( $('#custom_field_show_hours_yes').prop('checked') ? 'd/m/Y H:i' : 'd/m/Y' )," +
+              "this.setOptions( { format: ( $('#custom_field_show_hours_yes').prop('checked') ? 'Y-m-d H:i' : 'Y-m-d' )," +
               "timepicker: $('#custom_field_show_hours_yes').prop('checked') } );" +
               "} };" +
               "function datetimepickerCreate(id){" +
